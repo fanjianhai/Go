d3cfccce161f994d13ecaef925e9318f0372fbc5
范建海 <594042358@qq.com>
2020-10-09 17:43:52 +0800

    add: 路由配置

---
+++ 01-Gin框架/codes/02-路由配置/ginDemo/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/fanjianhai/gostudy/01-Gin框架/codes/02-路由配置/ginDemo/config"
+	"github.com/fanjianhai/gostudy/01-Gin框架/codes/02-路由配置/ginDemo/router"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	gin.SetMode(gin.DebugMode) // 默认为 debug 模式，设置为发布模式
+	engine := gin.Default()
+	router.InitRouter(engine) // 设置路由
+	engine.Run(config.PORT)
+}
---
+++ 01-Gin框架/codes/02-路由配置/ginDemo/router/router.go
@@ -0,0 +1,44 @@
+package router
+
+import (
+	"fmt"
+	"github.com/fanjianhai/gostudy/01-Gin框架/codes/02-路由配置/ginDemo/common"
+	"github.com/fanjianhai/gostudy/01-Gin框架/codes/02-路由配置/ginDemo/controller/v1"
+	"github.com/fanjianhai/gostudy/01-Gin框架/codes/02-路由配置/ginDemo/controller/v2"
+	"github.com/gin-gonic/gin"
+	"net/url"
+	"strconv"
+)
+
+func InitRouter(r *gin.Engine)  {
+
+	r.GET("/sn", SignDemo)
+
+	// v1 版本
+	GroupV1 := r.Group("/v1")
+	{
+		GroupV1.Any("/product/add", v1.AddProduct)
+		GroupV1.Any("/member/add", v1.AddMember)
+	}
+
+	// v2 版本
+	GroupV2 := r.Group("/v2", common.VerifySign)
+	{
+		GroupV2.Any("/product/add", v2.AddProduct)
+		GroupV2.Any("/member/add", v2.AddMember)
+	}
+}
+
+func SignDemo(c *gin.Context) {
+	ts := strconv.FormatInt(common.GetTimeUnix(), 10)
+	res := map[string]interface{}{}
+	params := url.Values{
+		"name"  : []string{"a"},
+		"price" : []string{"10"},
+		"ts"    : []string{ts},
+	}
+	res["sn"] = common.CreateSign(params)
+	fmt.Println(res["sn"])
+	res["ts"] = ts
+	common.RetJson("200", "", res, c)
+}
